07603d26f7db56d7699927fb04cc773b1e2d74da
kyulgon <kyulgon@naver.com>
2020-07-08 16:32:11 +0900

    Player 이동, 시네머신(카메라)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -12,20 +12,32 @@ public class PlayerMovement : MonoBehaviour {
 
     private void Start() {
         // 사용할 컴포넌트들의 참조를 가져오기
+        playerInput = GetComponent<PlayerInput>();
+        playerRigidbody = GetComponent<Rigidbody>();
+        playerAnimator = GetComponent<Animator>();
     }
 
     // FixedUpdate는 물리 갱신 주기에 맞춰 실행됨
     private void FixedUpdate() {
         // 물리 갱신 주기마다 움직임, 회전, 애니메이션 처리 실행
+        Rotate();
+        Move();
+
+        playerAnimator.SetFloat("Move", playerInput.move); // 입력값에 따라 애니메이터의 Move 파라미터값 변경
     }
 
     // 입력값에 따라 캐릭터를 앞뒤로 움직임
     private void Move() {
+        Vector3 moveDistance =
+            playerInput.move * transform.forward * moveSpeed * Time.deltaTime; // 상대적으로 이동할 거리 계산
 
+        playerRigidbody.MovePosition(playerRigidbody.position + moveDistance); // 리지디바디를 이용해 게임 오브젝트 위치 변경(현재위치 +상대적으로 더 이동할 거리)
     }
 
     // 입력값에 따라 캐릭터를 좌우로 회전
     private void Rotate() {
+        float turn = playerInput.rotate * rotateSpeed * Time.deltaTime; // 상대적으로 회전할 수치 계산
 
+        playerRigidbody.rotation = playerRigidbody.rotation * Quaternion.Euler(0, turn, 0f); // 리지드바디를 이용해 게임 오브젝트 회전 변경
     }
 }
